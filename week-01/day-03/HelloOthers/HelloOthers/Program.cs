b408bea5d3a3d0cf70eab18bf5ef8d9dd0e262bc
Tomas Janacek <tomasjanacek@hotmail.cz>
2021-09-08 13:28:40 +0200

    First three exercises done

---
+++ week-01/day-03/HelloOthers/HelloOthers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Honza!");
+            Console.WriteLine("Hello, Lucka!");
+            Console.WriteLine("Hello, Peter!");
+        }
+    }
+}
