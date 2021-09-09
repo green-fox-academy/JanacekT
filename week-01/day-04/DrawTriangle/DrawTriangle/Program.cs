e5fd7296f66c15cc6e90f96310b6e747c5b749a4
Tomas Janacek <tomasjanacek@hotmail.cz>
2021-09-09 21:55:18 +0200

    Tringle done

---
+++ week-01/day-04/DrawTriangle/DrawTriangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DrawTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+
+            Console.WriteLine("Dear Friend, please enter the number you wish and I will draw the triangle for you");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+
+            for (int i = 1; i <= a; i++)
+            {
+              
+                for (int b = 1; b<=i; b++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine(" ");
+            }
+
+
+        }
+    }
+}
