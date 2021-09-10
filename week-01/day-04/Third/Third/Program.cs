276be8dac4dcd1acf4bf2e5fd23192d334443dd1
Tomas Janacek <tomasjanacek@hotmail.cz>
2021-09-10 11:03:06 +0200

    Small update

---
+++ week-01/day-04/Third/Third/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 3, 5 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
