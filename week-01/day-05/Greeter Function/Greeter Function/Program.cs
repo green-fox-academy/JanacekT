e5384f2846e85394f9e8efe65fa0f55c825be85f
Tomas Janacek <tomasjanacek@hotmail.cz>
2021-09-12 21:20:12 +0200

    Functions

---
+++ week-01/day-05/Greeter Function/Greeter Function/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Greeter_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `al` and assign the value `Green Fox` to it
+            // - Create a function called `Greet()` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Green Fox`
+            // - Greet `al`
+
+            string al = " Green Fox";
+
+            Greet(al);
+        }
+
+        static void Greet(string al)
+        {
+            Console.WriteLine("Greetings dear, " + al);
+        }
+    }
+}
---
+++ week-01/day-05/Summing/Summing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Summing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function called `sum()` that returns the sum of numbers from zero to the given parameter
+
+            Console.WriteLine("Please, enter a number");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Sum(input));
+        }
+
+        static int Sum(int num)
+        {
+            int result = 0;
+
+            for (int i = 0; i <= num ; i++)
+            {
+                result += i;
+            }
+
+            return result;
+        }
+    }
+}
