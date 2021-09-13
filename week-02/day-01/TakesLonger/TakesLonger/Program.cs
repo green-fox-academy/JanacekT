4050cd5b883a68e54eed1bba3b2c306bec17bb0e
Tomas Janacek <tomasjanacek@hotmail.cz>
2021-09-13 13:40:35 +0200

    First day of second week

---
+++ week-02/day-01/TakesLonger/TakesLonger/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace TakesLonger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+            // When saving this quote a disk error has occured. Please fix it!
+            // Insert the words "always takes longer than" between the words "It" and "you" using the StringBuilder class!
+            // Use pieces of the quote variable (instead of just redefining the string)!
+
+            StringBuilder sb = new StringBuilder("Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.");
+            sb.Insert(quote.ToString().IndexOf("you "), "always takes longer than ");
+
+
+            Console.WriteLine(sb.ToString());
+
+            
+        }
+    }
+}
---
+++ week-02/day-01/ToDoList/ToDoList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText applying indentation
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+            StringBuilder sb = new StringBuilder(" - Buy milk\n");
+            sb.AppendLine("\n");
+
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
